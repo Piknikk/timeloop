9f010aa16e76fa9d209a78d24f8d029f9b6939d8
sydro <sydro@LAPTOP-6G0C07BV>
2021-09-05 13:57:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace timeloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= number; x++)
+                Console.WriteLine(x + " Abracadabra");
+
+        }
+    }
+}
